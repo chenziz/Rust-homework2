26951a747a7cd5fe1e3a870867933133d52f8cd2
chenziz <51749330+chenziz@users.noreply.github.com>
2021-06-08 23:05:07 +0800

    Update main2.rs

--- main2.rs
+++ main2.rs
@@ -1,3 +1,12 @@
+pub fn sum_u32(arr: &[u32]) -> Option<u32> {
+    arr.iter().try_fold(0u32,|acc,&x| acc.checked_add(x))
+}
+
+pub fn test_u32() {
+    let arr1: [u32; 10] = [1,2,3,4,5,6,7,8,9,10];
+    assert_eq!(Some(55), sum_u32(&arr1));
+}
+
 fn main() {
-    println!("Hello, world!");
+    test_u32();
 }
